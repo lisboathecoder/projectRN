c893f666cfadd824cbe07f6dc5dcea0bde20e5c2
lisboathecoder <gustavo.lisboa2009@gmail.com>
2026-09-14 11:43:24 -0300

    finish delete tab

--- app/(tabs)/_layout.js
+++ app/(tabs)/_layout.js
@@ -2,45 +2,50 @@ import { Tabs } from "expo-router";
 
 export default function TabsLayout() {
   return (
-    <Tabs
-      screenOptions={{
-        headerShadowVisible: false,
-        tabBarActiveTintColor: "#fe0f0f",
-        tabBarLabelStyle: {
-          fontSize: 12,
-          fontWeight: "600",
-          
-        },
-      }}
-    >
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: "Início",
-          headerTitle: "Projeto Base",
-        }}
-      />
-      <Tabs.Screen
-        name="post"
-        options={{
-          title: "Criar",
-          headerTitle: "Criar Animes",
-        }}
-      />
-            <Tabs.Screen
-        name="get"
-        options={{
-          title: "Buscar",
-          headerTitle: "Buscar Animes",
-        }}
-      />
-            <Tabs.Screen
-        name="sobre"
-        options={{
-          title: "Sobre",
-          headerTitle: "Sobre",
-        }}
-      />
-    </Tabs>
+      <Tabs
+          screenOptions={{
+              headerShadowVisible: false,
+              tabBarActiveTintColor: '#fe0f0f',
+              tabBarLabelStyle: {
+                  fontSize: 12,
+                  fontWeight: '600',
+              },
+          }}>
+          <Tabs.Screen
+              name="index"
+              options={{
+                  title: 'Início',
+                  headerTitle: 'Projeto Base',
+              }}
+          />
+          <Tabs.Screen
+              name="post"
+              options={{
+                  title: 'Criar',
+                  headerTitle: 'Criar Animes',
+              }}
+          />
+          <Tabs.Screen
+              name="get"
+              options={{
+                  title: 'Buscar',
+                  headerTitle: 'Buscar Animes',
+              }}
+          />
+          <Tabs.Screen
+              name="delete"
+              options={{
+                  title: 'Excluir',
+                  headerTitle: 'Excluir Animes',
+              }}
+          />
+          <Tabs.Screen
+              name="sobre"
+              options={{
+                  title: 'Sobre',
+                  headerTitle: 'Sobre',
+              }}
+          />
+      </Tabs>
   );
 }
